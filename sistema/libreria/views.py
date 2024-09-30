0d2977a9812e41d735877fff774a1c85f0b61185
kennedy <yerriber@gmail.com>
2024-09-29 23:01:06 -0500

    Primer commit

---
+++ sistema/libreria/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'paginas/inicio.html')
+def nosotros(request):
+    return render(request, 'paginas/nosotros.html')
+def Libros(request):
+    return render(request, 'libros/index.html')
